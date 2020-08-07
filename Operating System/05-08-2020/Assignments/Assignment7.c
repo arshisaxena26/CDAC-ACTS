def32405f118177235bbca87f37f79334f2ee944
arshisaxena26 <53929078+arshisaxena26@users.noreply.github.com>
2020-08-07 17:50:43 +0530

    Update Assignment7.c

--- Operating System/05-08-2020/Assignments/Assignment7.c
+++ Operating System/05-08-2020/Assignments/Assignment7.c
@@ -15,10 +15,13 @@ void *Table3(void *param);
 int main()
 {
 	pthread_t tid;
+	
 	pthread_attr_t attr;
 	pthread_attr_init(&attr);
+	
 	pthread_create(&tid, &attr, Table2, NULL);
 	pthread_create(&tid, &attr, Table3, NULL);
+	
 	pthread_join(tid,NULL);
 }
 
@@ -26,10 +29,10 @@ void *Table2(void *param)
 {
 	int i;
 	for(i=1;i<=10;i++)
-	{
 		printf("2 * %d = %d\n",i,2*i);
-		}
+		
 	printf("\n");	
+	
 	pthread_exit(0);
 	}
 
@@ -37,9 +40,9 @@ void *Table3(void *param)
 {
 	int i;
 	for(i=1;i<=10;i++)
-	{
 		printf("3 * %d = %d\n",i,3*i);
-		}
+		
 	printf("\n");	
+	
 	pthread_exit(0);
 	}

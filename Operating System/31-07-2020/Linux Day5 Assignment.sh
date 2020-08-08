ac4f6fbb1b94a09046266a837fbcda73a2bfadaf
arshisaxena26 <53929078+arshisaxena26@users.noreply.github.com>
2020-08-09 03:28:55 +0530

    Create Linux Day5 Assignment.sh

---
+++ Operating System/31-07-2020/Linux Day5 Assignment.sh
@@ -0,0 +1,35 @@
+1.Write a menu based script to perform following string operations
+a) To find length of a string
+c) Copying string
+d) Concatenation of strings
+e) Comparison of two strings
+f) Reversing a string
+
+2.Write a script to check whether given argument is a regular file or directory
+
+3.To check given year is leap or not
+
+4.Write a script to create 10 directories, say exam1,exam2,...,exam10 Report
+error if a directory/file exists with the same name.
+
+5.Write a script to check each entry of a directory is a file or directory.
+
+6.Write a script to find substring inside a string
+
+7.Write a script to find substring inside a file
+
+8.Write a script to store PRN no in an array:
+	a)Ask each student to store his/her personal info in file If his PRN no exists in the array
+	b)Give exit option 
+
+9.Write a script to replace occurrence of given string in file.
+
+10.Write a menu driven script(Use infinite loop for options.Program should ask for options continuously ):
+	a)shows ip address( use ip a)
+	b)shows free disk space (use df command)
+	c)shows free ram(use free command)
+	d)shows system info(use uname -a command)
+	e)list all files in home directory
+	f)list all files from /etc/ ends with .conf
+
+11.Write a menu driven file manager that lets you list,create and delete files or directories.

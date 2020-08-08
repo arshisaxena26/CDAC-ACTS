ca969d31bbe32c7a8113644d3a38ff8eb89fd98a
arshisaxena26 <53929078+arshisaxena26@users.noreply.github.com>
2020-08-09 03:49:32 +0530

    Update Linux Day 12 Assignment.sh

--- Operating System/08-08-2020/Linux Day 12 Assignment.sh
+++ Operating System/08-08-2020/Linux Day 12 Assignment.sh
@@ -9,16 +9,22 @@
 
 Optional:
 1.Run the firefox with highest priority.
+    • nice -n -20 firefox
 
 2. Run the firefox with lowest priority.
+    • renice -n +19 firefox
 
 3. Change the running firefox process priority to 10
+    • renice -n +10 firefox
 
 4.Use Grep to find all lines in the current directory starts with ‘a’.
+    • ls | grep ^a
 
 5.Use grep to find out all lines starts with ‘a’ and have numbers at second place and ends with txt
+    • ls | grep ^a[0-9]*.txt 
 
 6.Use grep to find out all lines start with ‘aaa’ and can’t have numbers in between.
+    • ls | grep ^aaa
 
 7.Write a program to create two threads:
 	a)Read data from file(Display newly added data on terminal)

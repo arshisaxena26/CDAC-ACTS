f1e53cfdb227a9ae009a0afb5a0dd87537319e77
arshisaxena26 <53929078+arshisaxena26@users.noreply.github.com>
2020-08-09 03:33:58 +0530

    Create Linux Day7 Assignment.sh

---
+++ Operating System/03-08-2020/Linux Day7 Assignment.sh
@@ -0,0 +1,28 @@
+1.Write a shell script to append the numbers in all filenames  of current directory(Don’t use home directory. )
+
+2.Write a menu driven script:
+	a)Store student name in array
+	b)Search name in array
+	c)Search by first name
+
+3. Write a menu driven script shell script that will:
+	a)print cpu information(read file /proc/cpuinfo)
+	b)print user details such as uid,gid,username,groups,home directory,default shell and his encrypted password(you can use uid,gid,groups commands and print    $HOME,$SHELL or try finding in /etc/passwd and /etc/shadow)
+
+4.Write a shell script to print all files permissions in current directory(Not name or other details)(Use cut commands)
+
+5.Write a shell script to print all files permissions and name of file
+
+6.Write a shell script to print all files name and size greater than 10K
+
+7.Write a shell script to automate tasks:(It’s not menu driven. Follow operations in order)
+	a)Install wget
+	b)Download file https://www.kernel.org/doc/Documentation/switchtec.txt
+	c)open file with vi editor(Let user edit the file and save it)
+https://www.kernel.org/doc/Documentation/switchtec.txt	d)Change all driver word occurrences  with DRIVER word in above file
+	e)Print it on terminal(wait for user to enter q to exit it)
+
+Optional:
+8.Write a shell script:
+	a)Download https://raw.githubusercontent.com/curran/data/gh-pages/all/geonames_cities10000000.csv using curl command
+   b) print name if population is greater than 12691835																																																																																				

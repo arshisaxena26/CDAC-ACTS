c50d425b3e94ffe2c00cf6454ec6220e0e2283d2
arshisaxena26 <53929078+arshisaxena26@users.noreply.github.com>
2020-08-09 03:39:29 +0530

    Create Linux Day 12 Assignment.sh

---
+++ Operating System/08-08-2020/Linux Day 12 Assignment.sh
@@ -0,0 +1,28 @@
+
+1. Write a program and do the following :using sigaction API, handle SIGINT, SIGTERM, SIGQUIT,SIGSTOP,SIGTSTP and SIGKILL. Install dummy handlers for the mentioned signals Using sigaction() system call API !!!
+
+2. Write a program to implement an unnamed semaphore on Day9 -7th question.
+
+3. Write a program to implement mutex on Day9-8th question.
+
+
+
+Optional:
+1.Run the firefox with highest priority.
+
+2. Run the firefox with lowest priority.
+
+3. Change the running firefox process priority to 10
+
+4.Use Grep to find all lines in the current directory starts with ‘a’.
+
+5.Use grep to find out all lines starts with ‘a’ and have numbers at second place and ends with txt
+
+6.Use grep to find out all lines start with ‘aaa’ and can’t have numbers in between.
+
+7.Write a program to create two threads:
+	a)Read data from file(Display newly added data on terminal)
+	b)Write data to file(Read data from user)
+	c)Do this without semaphore first(Analyze the output)
+	d)Implement semaphore(Analyze the output)
+	e)Now Copy the code and implement mutex(Analyze the output)(Takeout the semaphore  code)

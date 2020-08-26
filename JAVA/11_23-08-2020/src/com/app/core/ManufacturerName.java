015c11602f9cf4bd701ce4cb06ee6bfef607bae9
arshisaxena26 <53929078+arshisaxena26@users.noreply.github.com>
2020-08-27 04:26:46 +0530

    Update ManufacturerName.java

--- JAVA/11_23-08-2020/src/com/app/core/ManufacturerName.java
+++ JAVA/11_23-08-2020/src/com/app/core/ManufacturerName.java
@@ -2,10 +2,4 @@ package com.app.core;
 
 public enum ManufacturerName {
 	MARUTI, HONDA, HYUNDAI;
-
-	// Overriding toString to return Manufacturer Name
-	@Override
-	public String toString() {
-		return name();
-	}
 }

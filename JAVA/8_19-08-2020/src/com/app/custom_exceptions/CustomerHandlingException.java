d853cf64e6ad1d1553298577c24f269acebb0f5a
arshisaxena26 <arshisaxena26@gmail.com>
2020-09-01 21:35:17 +0530

    Files Updated

--- JAVA/8_19-08-2020/src/com/app/custom_exceptions/CustomerHandlingException.java
+++
@@ -1,8 +0,0 @@
-package com.app.custom_exceptions;
-
-@SuppressWarnings("serial")
-public class CustomerHandlingException extends Exception {
-	public CustomerHandlingException(String errorMsg) {
-		super(errorMsg);
-	}
-}

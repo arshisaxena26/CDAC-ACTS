01383f6de8a33fd0594fb200050833e6c817f40b
arshisaxena26 <arshisaxena26@gmail.com>
2020-11-12 00:31:35 +0530

    Files Added

---
+++ MEAN/01_09 .11.2020/Assignment/Question_07/cmi.js
@@ -0,0 +1,7 @@
+/* Question :  Write a program cmi.js that accepts a distance in inches and prints the corresponding value in cms.
+				 Note that 1 inch = 2.54 cm. Test the js file in HTML.
+*/
+
+function distanceCal(distInches){
+	document.write("Distance in cms for " + distInches + " inches : " + (distInches * 2.54));
+}

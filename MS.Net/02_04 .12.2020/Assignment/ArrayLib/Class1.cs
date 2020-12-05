8580d5414b5b78b0c69cad446cc8d3d8df77ad1e
arshisaxena26 <arshisaxena26@gmail.com>
2020-12-05 21:36:00 +0530

    Files Added

---
+++ MS.Net/02_04 .12.2020/Assignment/ArrayLib/Class1.cs
@@ -0,0 +1,128 @@
+﻿using System;
+
+namespace ArrayLib
+{
+    public class ArrayFunctions
+    {
+        public int[] bubbleSort(int[] numbers)
+        {
+            for (int i = 0; i < numbers.Length - 1; i++)
+            {
+                for (int j = 0; j < numbers.Length - i - 1; j++)
+                {
+                    if (numbers[j] > numbers[j + 1])
+                    {
+                        int temp = numbers[j];
+                        numbers[j] = numbers[j + 1];
+                        numbers[j + 1] = temp;
+                    }
+                }
+            }
+            return numbers;
+        }
+
+        int partition(int[] numbers, int low, int high)
+        {
+            int pivot = numbers[high];
+
+            int i = (low - 1);
+            for (int j = low; j < high; j++)
+            {
+                if (numbers[j] < pivot)
+                {
+                    i++;
+
+                    int temp = numbers[i];
+                    numbers[i] = numbers[j];
+                    numbers[j] = temp;
+                }
+            }
+
+            int temp1 = numbers[i + 1];
+            numbers[i + 1] = numbers[high];
+            numbers[high] = temp1;
+
+            return i + 1;
+        }
+
+        public int[] quickSort(int[] nums, int low, int high)
+        {
+            if (low < high)
+            {
+                int pi = partition(nums, low, high);
+
+                quickSort(nums, low, pi - 1);
+                quickSort(nums, pi + 1, high);
+            }
+            return nums;
+        }
+
+        public void linearSearch(int[] numbers, int num)
+        {
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (numbers[i] == num)
+                {
+                    Console.WriteLine("Found");
+                    return;
+                }
+            }
+            Console.WriteLine("Not Found");
+        }
+
+        public void binarySearch(int[] nums, int key)
+        {
+            int min = 0;
+            int max = nums.Length - 1;
+            while (min <= max)
+            {
+                int mid = (min + max) / 2;
+                if (key == nums[mid])
+                {
+                    Console.WriteLine("Found");
+                    return;
+                }
+                else if (key < nums[mid])
+                {
+                    max = mid - 1;
+                }
+                else
+                {
+                    min = mid + 1;
+                }
+            }
+            Console.WriteLine("Not Found");
+        }
+
+        public void binarySearchRecursive(
+            int[] numbers,
+            int key,
+            int min,
+            int max
+        )
+        {
+            if (min > max)
+            {
+                Console.WriteLine("Not Found");
+                return;
+            }
+            else
+            {
+                int mid = (min + max) / 2;
+                if (key == numbers[mid])
+                {
+                    Console.WriteLine("Found");
+                    return;
+                }
+                else if (key < numbers[mid])
+                {
+                    binarySearchRecursive(numbers, key, min, mid - 1);
+                }
+                else
+                {
+                    binarySearchRecursive(numbers, key, mid + 1, max);
+                }
+            }
+        }
+    }
+}
---
+++ MS.Net/02_04 .12.2020/Assignment/ArrayTester/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using ArrayLib;
+
+namespace ArrayTester
+{
+    class ArrayTest
+    {
+        static void printArray(int[] nums)
+        {
+            for (int i = 0; i < nums.Length; i++) Console.Write(nums[i] + " ");
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the size of Array");
+            int size = Int32.Parse(Console.ReadLine());
+
+            int[] numbers = new int[size];
+
+            Console.WriteLine("Enter " + size + " numbers");
+            for (int i = 0; i < size; i++)
+            {
+                numbers[i] = Int32.Parse(Console.ReadLine());
+            }
+
+            ArrayFunctions arrayfunc = new ArrayFunctions();
+
+            Console.WriteLine("\nSorted Using BubbleSort");
+            int[] result = arrayfunc.bubbleSort(numbers);
+            printArray (result);
+
+            Console.WriteLine("\nSorted Using QuickSort");
+            result = arrayfunc.quickSort(numbers, 0, numbers.Length - 1);
+            printArray (result);
+
+            Console.WriteLine("\n\nEnter number to search");
+            int searchNum = Int32.Parse(Console.ReadLine());
+
+            Console.WriteLine("\nSearching Using Linear Search");
+            arrayfunc.linearSearch (numbers, searchNum);
+
+            Console.WriteLine("\nSorted Using Binary Search");
+            arrayfunc.binarySearch (result, searchNum);
+
+            Console.WriteLine("\nSorted Using Binary Search with Recursion");
+            arrayfunc
+                .binarySearchRecursive(result, searchNum, 0, result.Length - 1);
+        }
+    }
+}

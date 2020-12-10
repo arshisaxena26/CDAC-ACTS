acc241bde41e2b45036ee1d6820854d713486011
arshisaxena26 <arshisaxena26@gmail.com>
2020-12-10 23:35:51 +0530

    Files Added

---
+++ MS.Net/04_07.12.2020/Assignment2/ReflectionTest/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Reflection; 
+
+namespace ReflectionTest
+{
+    class ReflectionClass
+    {
+        public string msg;
+
+        private ReflectionClass()
+        {
+            this.msg = "Private Constructor Accessed Using Reflection";
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var constructor = typeof(ReflectionClass).GetConstructor(BindingFlags.NonPublic|BindingFlags.Instance, null, new Type[0], null);
+            var instance = (ReflectionClass)constructor.Invoke(null);
+            Console.WriteLine(instance.msg);
+        }
+    }
+}

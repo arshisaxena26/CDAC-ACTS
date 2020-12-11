1f45f20d97e67a5928083c4c283b0e11597315e1
arshisaxena26 <53929078+arshisaxena26@users.noreply.github.com>
2020-12-11 21:11:37 +0530

    Add files via upload

---
+++ MS.Net/05_08.12.2020/Assignment1/EmployeeWebApp/Controllers/EmployeeController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using EmployeeWebApp.Models;
+
+namespace EmployeeWebApp.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        [HttpGet]
+        public ActionResult CreateEmployee()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult CreateEmployee(Employee emp)
+        {
+            Response.Write("Employee ID: "+emp.EmpId+"Employee Name: "+emp.EmpName+
+                "Employee Gender: "+emp.EmpGender+"Employee Phone: "+emp.EmpPhone);
+            return View();
+        }
+    }
+}
---
+++ MS.Net/05_08.12.2020/Assignment1/EmployeeWebApp/Models/Employee.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EmployeeWebApp.Models
+{
+    public class Employee
+    {
+        public int EmpId { get; set; }
+        public string EmpName { get; set; }
+        public string EmpGender { get; set; }
+        public string EmpPhone { get; set; }
+    }
+}

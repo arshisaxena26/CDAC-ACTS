e48ac496a39ec39effd95b543cb91cd664ad88fe
arshisaxena26 <53929078+arshisaxena26@users.noreply.github.com>
2020-12-12 15:31:49 +0530

    Add files via upload

---
+++ MS.Net/06_09.12.2020/Assignment3/StronglyTypedViews/Controllers/EmployeeController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using StronglyTypedViews.Models;
+
+namespace StronglyTypedViews.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        [HttpGet]
+        public ActionResult EmpInfo()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult EmpInfo(Employee emp)
+        {
+            Response.Write(" Employee ID : " + emp.Id + " Employee Name : " + emp.Name + " Employee Gender: " + emp.Gender + 
+                " Employee Phone : " + emp.Phone + " Employee Address : " + emp.Address);
+            return View();
+        }
+    }
+}
---
+++ MS.Net/06_09.12.2020/Assignment3/StronglyTypedViews/Models/Employee.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace StronglyTypedViews.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Gender { get; set; }
+        public string Phone { get; set; }
+
+        [DataType(DataType.MultilineText)]
+        public string Address { get; set; }
+    }
+}

a1a53d4d14fececa079007caf269e85b4d846064
arshisaxena26 <53929078+arshisaxena26@users.noreply.github.com>
2020-12-15 16:20:03 +0530

    Add files via upload

---
+++ MS.Net/09_12.12.2020/Assignment1/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace AdoNetConnectedMode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            User u = new User();
+            u.BindDataConnectedMode();
+            Console.ReadLine();
+        }
+
+    }
+
+    public class User
+    {
+        public void BindDataConnectedMode()
+        {
+            SqlConnection con = null;
+            SqlCommand command = null;
+            try
+            {
+                con = new SqlConnection("Data Source=.;Initial Catalog=user_schema; Integrated Security = true;");
+                command = new SqlCommand("select * from Users", con);
+                con.Open();
+                SqlDataReader reader = command.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    Console.WriteLine(reader["Id"] + " " + reader["Name"] + " " + reader["Address"] + " " + reader["Contact"]);
+                }
+            }
+            catch (Exception e)
+            {
+		 Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                con.Close();
+                command.Dispose();
+            }
+        }
+    }
+}
---
+++ MS.Net/09_12.12.2020/Assignment2/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdoNetDisconnectedMode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            User u = new User();
+            u.BindDataDisconnectedMode();
+            Console.ReadLine();
+        }
+    }
+    public class User
+    {
+        public void BindDataDisconnectedMode()
+        {
+            SqlConnection con = null;
+
+            try
+            {
+                con = new SqlConnection("Data Source=.;Initial Catalog=user_schema; Integrated Security = true;");
+                SqlDataAdapter da = new SqlDataAdapter("select * from Users", con);
+                DataSet ds = new DataSet();
+                da.Fill(ds);
+
+                if(ds != null && ds.Tables.Count !=0 && ds.Tables[0].Rows.Count > 0)
+                {
+                    for(int i=0; i < ds.Tables[0].Rows.Count; i++)
+                    {
+                        string id = ds.Tables[0].Rows[i]["Id"].ToString();
+                        string name = ds.Tables[0].Rows[i]["Name"].ToString();
+                        string address = ds.Tables[0].Rows[i]["Address"].ToString();
+                        string contact = ds.Tables[0].Rows[i]["Contact"].ToString();
+
+                        Console.WriteLine(id+" "+name+" "+address+" "+contact);
+                    }
+                }
+              
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}

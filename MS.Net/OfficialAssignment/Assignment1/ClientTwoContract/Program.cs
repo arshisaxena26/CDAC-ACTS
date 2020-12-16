0e0411955916bbe9b36e723390042c2aa14242ea
arshisaxena26 <53929078+arshisaxena26@users.noreply.github.com>
2020-12-16 15:52:42 +0530

    Add files via upload

---
+++ MS.Net/OfficialAssignment/Assignment1/ClientTwoContract/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ClientTwoContract.ServiceReference1;
+
+namespace ClientTwoContract
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Service1Client client1 = new Service1Client();
+            int sum = client1.Add(10, 20);
+            int sub = client1.Sub(20, 10);
+            int mul = client1.Mul(2, 3);
+            int div = client1.Div(10, 5);
+
+            Console.WriteLine("Addition : " + sum);
+            Console.WriteLine("Subtraction : " + sub);
+            Console.WriteLine("Multiplication : " + mul);
+            Console.WriteLine("Division : " + div);
+
+            Service2Client client2 = new Service2Client();
+            Console.WriteLine(client2.Name("John"));
+            Console.WriteLine(client2.Email("john@gmail.com"));
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ MS.Net/OfficialAssignment/Assignment1/WcfServiceTwoContract/Service1.svc.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace WcfServiceTwoContract
+{
+    public class Service1 : IService1, IService2
+    {
+        public int Add(int a, int b)
+        {
+            return a + b;
+        }
+
+        public int Div(int a, int b)
+        {
+            return a / b;
+        }
+
+        public string Email(string email)
+        {
+            return email;
+        }
+
+        public int Mul(int a, int b)
+        {
+            return a * b;
+        }
+
+        public string Name(string name)
+        {
+            return name;
+        }
+
+        public int Sub(int a, int b)
+        {
+            return a - b;
+        }
+    }
+}

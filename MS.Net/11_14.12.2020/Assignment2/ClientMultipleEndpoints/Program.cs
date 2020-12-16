3a9e4e1b9b18efb46e53bbb095eda27c41c30dc7
arshisaxena26 <53929078+arshisaxena26@users.noreply.github.com>
2020-12-16 13:56:18 +0530

    Add files via upload

---
+++ MS.Net/11_14.12.2020/Assignment2/ClientMultipleEndpoints/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ClientMultipleEndpoints.ServiceReference1;
+
+namespace ClientMultipleEndpoints
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Service1Client proxy = null;
+
+            proxy = new Service1Client("FirstBinding");
+            Console.WriteLine(proxy.GetMessage(" First End Point"));
+
+            proxy = new Service1Client("SecondBinding");
+            Console.WriteLine(proxy.GetMessage(" Second End Point"));
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ MS.Net/11_14.12.2020/Assignment2/WcfServiceMultipleEndpoints/Service1.svc.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace WcfServiceMultipleEndpoints
+{
+    public class Service1 : IService1
+    {
+        public string GetMessage(string name)
+        {
+            return "Calling From" + name;
+        }
+    }
+}

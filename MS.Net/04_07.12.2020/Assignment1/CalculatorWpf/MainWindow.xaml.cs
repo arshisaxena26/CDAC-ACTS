09b27e1ff668e3311ed0198e685a2f5ff9d54f4e
arshisaxena26 <53929078+arshisaxena26@users.noreply.github.com>
2020-12-17 01:37:46 +0530

    Add files via upload

---
+++ MS.Net/04_07.12.2020/Assignment1/CalculatorWpf/MainWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CalculatorWpf
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        public void AddButton(object sender, RoutedEventArgs e)
+        {
+            Button b = (Button)sender;
+            ResultInput.Text += Convert.ToInt32(FirstNumInput.Text) + Convert.ToInt32(SecondNumInput.Text);
+        }
+    }
+}

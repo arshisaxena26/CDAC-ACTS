a81ef4de95b1caa954c7cad3a22788022e2eb1f3
arshisaxena26 <53929078+arshisaxena26@users.noreply.github.com>
2020-12-16 15:37:05 +0530

    Add files via upload

---
+++ MS.Net/11_14.12.2020/Assignment4/WcfServiceTwoContract/IService2.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WcfServiceTwoContract
+{
+    [ServiceContract]
+    public interface IService2
+    {
+        [OperationContract]
+        string Name(string name);
+
+        [OperationContract]
+        string Email(string email);
+    }
+}
